c752db5eec7da87de39cdccfa1ec6fed442ca9ac
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-07-02 22:30:55 -0400

    Create Binary_Emitter.cpp

---
+++ Binary_Emitter.cpp
@@ -0,0 +1,14 @@
+// BinaryEmitter.h
+#pragma once
+#include <cstdlib>
+#include <string>
+
+class BinaryEmitter {
+public:
+    void assemble(const std::string& asmFile, const std::string& exeFile) {
+        std::string nasmCmd = "nasm -f win64 " + asmFile + " -o output.obj";
+        std::string linkCmd = "link /subsystem:console /entry:main output.obj /OUT:" + exeFile;
+        system(nasmCmd.c_str());
+        system(linkCmd.c_str());
+    }
+};
